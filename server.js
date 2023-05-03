d9c72b122714cb93ed4a69843a78c526f7236b86
Sudip Shrestha <sudip.kumar.shrestha@vesuvio.io>
2023-05-04 00:25:30 +0545

    mongo db connect

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express=require('express');
 
+
 const app=express();
 const PORT=process.env.PORT||8080;
+
+
+// creating raute
 app.get('/',(req,res)=>{
-
     res.send("Crud app loi loi");
-
 });
 
+app.get('/login',(req,res)=>{
+    res.send
+});
 app.listen(PORT,()=>console.log(`server is running on http://localhost:${PORT} `));
