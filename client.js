29af8e823b52e0871a93d00ddd7672640cbabfc2
Sudip Shrestha <sudip.kumar.shrestha@vesuvio.io>
2023-05-05 11:35:09 +0545

    asdf

---
+++ client.js
@@ -0,0 +1,18 @@
+//const io=require('socket.io')(8000)
+
+// const users={};
+
+
+// io.on('connection',socket=>{
+//     socket.on('newUserJoin',name=>{
+//         users[socket.id]=name;
+//         socket.broadcast.emit('userJoined',name);
+//     });
+
+//     socket.on('send',message=>{
+//         socket.broadcast.emit('receive',{message:message,name:user[socket.id]});
+//     });
+
+// });
+
+const socket=io
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-// const express=require('express');
-// const app=express();
-// const PORT=process.env.PORT||8080;
-// app.get('/',(req,res)=>{
-//     res.sendFile(__dirname+'/views/index.html');
-// });
-// app.listen(PORT,()=>console.log(`server is running on http://localhost:${PORT} `));
-
-
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path= require('path');
 const PORT=process.env.PORT||3000;
-
-const http=require('http').createServer(app);
-
-http.listen(PORT,()=>{
-console.log(` server is running on http://localhost:${PORT}`);
-
-})
 const staticPath=path.join(__dirname,'assets');
 app.use(express.static(staticPath));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname+'/views/index.html');
-    // res.writeHead(200,{'Content-Type':'text/html'});
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('send', (msg) => {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('receive',msg);
+  });
+});
+
+server.listen(PORT, () => {
+    console.log(` server is running on http://localhost:${PORT}`);
+});
+
+
